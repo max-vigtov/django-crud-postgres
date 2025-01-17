f4e3f3e10863033a24a9f661f6d4d9ddd319876a
Max Vigueras <mvt.2000@hotmail.com>
2025-01-17 17:55:53 -0600

    Tasks: Show & Delete Features added

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,16 @@ from .models import Task
 
 # Create your views here.
 def list_tasks( request ):
-	return render( request, 'list_tasks.html' )
+	tasks = Task.objects.all()
+	print(tasks)
+	return render( request, 'list_tasks.html', { "tasks": tasks })
 
 def create_task( request ):
 	task = Task(title = request.POST['title'], description = request.POST['description'])
 	task.save()
+	return redirect('/tasks/')
+
+def delete_task(request, task_id):
+	taskToDelete = Task.objects.get(id=task_id)
+	taskToDelete.delete()
 	return redirect('/tasks/')
